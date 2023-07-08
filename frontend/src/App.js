f67bda43a18edd1e4f31b7f72c7d2da52b66210d
tylaraynes <70238175+tylaraynes@users.noreply.github.com>
2023-07-07 21:10:50 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import './App.css';
 
-import {BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import LoginPage from './pages/LoginPage';
 import CardPage from './pages/CardPage';
 
 function App() {
   return (
-    <Router>
-    <Switch>
-      <Route path="/" exact>
-        <LoginPage />
-        </Route>
-      <Route path="/cards" exact>
-        <CardPage />
-    </Route>
-    <Redirect to="/" />
-    </Switch>
-  </Router>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" index element={<LoginPage />} />
+      <Route path="/cards" index element={<CardPage />} />
+    </Routes>
+  </BrowserRouter>
 );
 }
 

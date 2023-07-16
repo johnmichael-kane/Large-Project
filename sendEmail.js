c9a0118858f68d459f27debc8a1bb880ffcd135d
tylaraynes <70238175+tylaraynes@users.noreply.github.com>
2023-07-16 17:51:51 -0400

    File for emails via gmail

---
+++ sendEmail.js
@@ -0,0 +1,33 @@
+const nodemailer = require("nodemailer");
+const handlebars = require("handlebars");
+const fs = require("fs");
+const path = require("path");
+
+const sendEmail = async (email, link) => {
+
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'nutritionapp7@gmail.com',
+      pass: 'afzttirvjllddhre'
+    }
+  });
+  
+
+  var mailOptions = {
+    from: 'nutritionapp7@gmail.com',
+    to: email,
+    subject: 'Password Reset',
+    text: 'This is you password reset link: http://' + link,
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  }); 
+};
+
+module.exports = sendEmail;

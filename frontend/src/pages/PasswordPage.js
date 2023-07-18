1d42e2aa8d0995d70444e04a6236bd522b2944e5
tylaraynes <70238175+tylaraynes@users.noreply.github.com>
2023-07-18 15:48:39 -0400

    Add files via upload

---
+++ frontend/src/pages/PasswordPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import PageTitle from '../components/PageTitle';
+import PasswordReset from '../components/PasswordReset';
+
+const PasswordPage = () =>
+{
+    return(
+        <div>
+            <PageTitle />
+            <PasswordReset />
+        </div>
+    );
+}
+
+export default PasswordPage;

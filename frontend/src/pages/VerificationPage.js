b04a5c9bc0a81f886fb1e8919b4d700649a0ca40
tylaraynes <70238175+tylaraynes@users.noreply.github.com>
2023-07-18 14:30:20 -0400

    Add email verification page

---
+++ frontend/src/pages/VerificationPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import PageTitle from '../components/PageTitle';
+import EmailVerification from '../components/EmailVerification';
+
+const VerificationPage = () =>
+{
+
+    return(
+      <div>
+        <PageTitle />
+        <EmailVerification/>
+      </div>
+    );
+};
+
+export default VerificationPage;

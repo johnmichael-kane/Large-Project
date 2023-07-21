dd123d2ffeb2688cf9bcaeeefb93d34306c89bc8
da-baby-car <107286931+da-baby-car@users.noreply.github.com>
2023-07-20 22:49:08 -0400

    Update api.js

--- api.js
+++ api.js
@@ -303,8 +303,8 @@ app.post('/api/register', async (req, res, next) =>
         error = 'exists';
     }
     else{
-      const hash = await bcrypt.hash(password, Number(process.env.BCRYPT_SALT));//take out if this breaks
-      const newUser = {Email: email, Password: password, EmailAuth: false};
+      const hash = await bcrypt.hash(password, Number(process.env.BCRYPT_SALT));//
+      const newUser = {Email: email, Password: hash, EmailAuth: false};
       const insert = await db.collection('Users').insertOne(newUser);
       error = 'created';
     }

24466f22bb40de9ea0dff313cd2a602a8e47c4c4
tylaraynes <70238175+tylaraynes@users.noreply.github.com>
2023-07-22 18:06:36 -0400

    Update api.tsx for accesstoken

--- mobile/API/api.tsx
+++ mobile/API/api.tsx
@@ -2,27 +2,30 @@ import axios, { AxiosResponse } from 'axios';
 
 export interface LoginResponse {
   Email: string;
+  accessToken: string;
   error: string;
 }
 
-export function login(Email: string, Password: string): Promise<LoginResponse> {
+export function login(email: string, password: string) : Promise<LoginResponse> {
   return new Promise((resolve, reject) => {
-    axios
-      .post<LoginResponse>('https://group7-largeproject-fcbd9bb42321.herokuapp.com/api/login', { Email, Password })
-      .then((response: AxiosResponse<LoginResponse>) => {
-        const data: LoginResponse = response.data;
-        if (data.error) {
-          console.log('Login Error:', data.error);
-          reject(new Error(data.error));
-        } else {
+  axios
+    .post<LoginResponse>('https://group7-largeproject-fcbd9bb42321.herokuapp.com/api/login', { email, password })
+    .then((response: AxiosResponse<LoginResponse>) => {
+      const data: LoginResponse = response.data;
+      if (data.error === "loginFailure") {
+        console.log('Login Error:', data.error);
+        resolve(data);
+      } else {
           console.log('Login successful!');
           console.log('User ID:', data.Email);
-          resolve(data);
-        }
-      })
-      .catch((error: any) => {
-        console.error('Error in login:', error);
-        reject(error);
-      });
-  });
+          console.log('Token ' + data.accessToken)
+        resolve(data);
+    
+      }
+    })
+    .catch((error: any) => {
+      console.error('Error in login:', error);
+      reject(error);
+    });
+});
 }

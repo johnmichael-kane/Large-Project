df5a8150dfa0c2fb1af930e2199434432f75cdd2
tylaraynes <70238175+tylaraynes@users.noreply.github.com>
2023-07-22 16:34:26 +0000

    Update api.tsx

--- mobile/API/api.tsx
+++ mobile/API/api.tsx
@@ -1,25 +1,22 @@
 import axios, { AxiosResponse } from 'axios';
 
-interface LoginResponse {
-  userId: number;
-  firstName: string;
-  lastName: string;
+export interface LoginResponse {
+  Email: string;
   error: string;
 }
 
-function login(Email: string, Password: string) {
+export function login(email: string, password: string) {
   axios
-    .post<LoginResponse>('https://group7-largeproject-fcbd9bb42321.herokuapp.com/api/login', { Email, Password })
+    .post<LoginResponse>('https://group7-largeproject-fcbd9bb42321.herokuapp.com/api/login', { email, password })
     .then((response: AxiosResponse<LoginResponse>) => {
       const data: LoginResponse = response.data;
       if (data.error) {
         console.error('Login Error:', data.error);
       } else {
-        const { userId, firstName, lastName } = data;
+        const { Email, error } = data;
         console.log('Login successful!');
-        console.log('User ID:', userId);
-        console.log('First Name:', firstName);
-        console.log('Last Name:', lastName);
+        console.log('User ID:', email);
+        console.log('error: ', error);
       }
     })
     .catch((error: any) => {

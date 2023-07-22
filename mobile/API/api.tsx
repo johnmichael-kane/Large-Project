5c6493fcf422dd17d59ef14116df4834dbce8dd7
da-baby-car <107286931+da-baby-car@users.noreply.github.com>
2023-07-22 13:14:17 -0400

    Update api.tsx

--- mobile/API/api.tsx
+++ mobile/API/api.tsx
@@ -5,22 +5,24 @@ export interface LoginResponse {
   error: string;
 }
 
-export function login(Email: string, Password: string) {
-  axios
-    .post<LoginResponse>('https://group7-largeproject-fcbd9bb42321.herokuapp.com/api/login', { Email, Password })
-    .then((response: AxiosResponse<LoginResponse>) => {
-      const data: LoginResponse = response.data;
-      if (data.error) {
-        console.log('Login Error:', data.error);
-      } else {
-        const { Email, error } = data;
-        console.log('Login successful!');
-        console.log('User ID:', email);
-        console.log('error: ', error);
-        return data;
-      }
-    })
-    .catch((error: any) => {
-      console.error('Error in login:', error);
-    });
+export function login(Email: string, Password: string): Promise<LoginResponse> {
+  return new Promise((resolve, reject) => {
+    axios
+      .post<LoginResponse>('https://group7-largeproject-fcbd9bb42321.herokuapp.com/api/login', { Email, Password })
+      .then((response: AxiosResponse<LoginResponse>) => {
+        const data: LoginResponse = response.data;
+        if (data.error) {
+          console.log('Login Error:', data.error);
+          reject(new Error(data.error));
+        } else {
+          console.log('Login successful!');
+          console.log('User ID:', data.Email);
+          resolve(data);
+        }
+      })
+      .catch((error: any) => {
+        console.error('Error in login:', error);
+        reject(error);
+      });
+  });
 }

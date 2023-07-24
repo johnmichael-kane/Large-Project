af0d3817d307f7151da9fbee4a0a11f80a9b8460
jordangenest <133993573+jordangenest@users.noreply.github.com>
2023-07-24 13:47:49 -0400

    Add files via upload
    Alternate stack navigation mobile app

--- mobilePages/App.tsx
+++ mobilePages/App.tsx
@@ -1,1 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import TabOneScreen from './screens/login';
+import TabTwoScreen from './screens/register';
+import BigList from './screens/biglist';
+import SettingsPage from './screens/settings';
 
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={TabOneScreen} />
+        <Stack.Screen name="Register" component={(TabTwoScreen)} />
+        <Stack.Screen name="BigList" component={BigList} />
+        <Stack.Screen name="Settings" component={SettingsPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

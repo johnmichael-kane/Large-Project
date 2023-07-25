452bd3f18ccf8759bda37d9ee84c07a8c65c4cd9
CandyKaine1 <123655765+CandyKaine1@users.noreply.github.com>
2023-07-25 15:55:50 -0400

    Update FoodListUI.js

--- frontend/src/components/FoodListUI.js
+++ frontend/src/components/FoodListUI.js
@@ -99,25 +99,54 @@ console.log("foods:", foods);
       </button>
       <span id="foodAddResult">{message}</span>
 
-      <div className="grid-container">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css"></link>
+            <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/dataTables.bootstrap5.min.css"></link>
+
+            <script defer src="https://code.jquery.com/jquery-3.7.0.js"></script>
+            <script defer src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+            <script defer src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
+            <script defer src="script.js"></script>
+            <table id="example" class="table" style={{width: '100%'}}>
+        <thead class="table-dark">
+            <tr>
+                <th>Food Name</th>
+                <th>Calories</th>
+                <th>Fats</th>
+                <th>Carbs</th>
+                <th>Protein</th>
+                <th>Servings</th>
+                <th> </th>
+            </tr>
+        </thead>
         {filteredFoods.length > 0 ? (
           filteredFoods.map((foodName, index) => (
-            <div key={index} className="grid-item">
-              <h3>{foodName}</h3>
-              <p>Calories: {foods.caloriesResults[index]}</p>
-              <p>Protein: {foods.proteinResults[index]}</p>
-              <p>Fats: {foods.fatResults[index]}</p>
-              <p>Carbs: {foods.carbsResults[index]}</p>
-              <p>Serving Size: {foods.servingResults[index]}</p>
-              <button onClick={(event) => addUserFood(event, foodName)}>
-                Add
-              </button>
-            </div>
-          ))
-        ) : (
-          <p>No matching foods found.</p>
-        )}
-      </div>
+                        <tr key={index}>
+                            <td>{foodName}</td>
+                            <td>{foods.caloriesResults[index]}</td>
+                            <td>{foods.proteinResults[index]}</td>
+                            <td>{foods.fatResults[index]}</td>
+                            <td>{foods.carbsResults[index]}</td>
+                            <td>{foods.servingResults[index]}</td>
+                            <td class="table-dark"><button onClick={(event) => addUserFood(event, foodName)}>Add</button></td>
+                        </tr>
+                    ))
+                    
+                )
+                 : (
+                    <p>No matching foods found.</p>
+                )}
+        <tfoot class="table-secondary">
+            <tr>
+                <th>Totals:</th>
+                <th>{mealPlan.totalCalories}</th>
+                <th>{mealPlan.Fats}</th>
+                <th>{mealPlan.Carbs}</th>
+                <th>{mealPlan.Protein}</th>
+                <th>Servings</th>
+                <th> </th>
+            </tr>
+        </tfoot>
+    </table>
     </div>
   );
 }

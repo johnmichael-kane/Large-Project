1d7b4c49546ba241d965bd450a48a768693eb3bf
J G <zappiepo@aol.com>
2023-07-25 23:14:29 -0400

    Update App.tsx

--- mobilePages/App.tsx
+++ mobilePages/App.tsx
@@ -1,19 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import TabOneScreen from './screens/login';
-import TabTwoScreen from './screens/register';
-import BigList from './screens/biglist';
-import SettingsPage from './screens/settings';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import TabOneScreen from "./screens/login";
+import TabTwoScreen from "./screens/register";
+import BigList from "./screens/biglist";
+import SettingsPage from "./screens/settings";
+import MealPlanPage from "./screens/mealplanpage";
+import { LogBox } from "react-native";
+import RequestPasswordReset from "./screens/requestpasswordreset";
+//this turns off all warnings, only turn this on if necessary
+LogBox.ignoreAllLogs(true);
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator 
+      <Stack.Navigator
         initialRouteName="Login"
         screenOptions={{
           headerStyle: {
@@ -23,8 +27,13 @@ export default function App() {
         }}
       >
         <Stack.Screen name="Login" component={TabOneScreen} />
-        <Stack.Screen name="Register" component={(TabTwoScreen)} />
+        <Stack.Screen name="Register" component={TabTwoScreen} />
         <Stack.Screen name="BigList" component={BigList} />
+        <Stack.Screen name="MealPlanPage" component={MealPlanPage} />
+        <Stack.Screen
+          name="RequestPasswordReset"
+          component={RequestPasswordReset}
+        />
         <Stack.Screen name="Settings" component={SettingsPage} />
       </Stack.Navigator>
     </NavigationContainer>
@@ -34,8 +43,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });

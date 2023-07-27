2a75639446172a26544dcfd0750dd1f91d4fe4e3
Abdul <64501953+Abdul4154@users.noreply.github.com>
2023-07-26 21:20:30 -0400

    Gave custom top bar

--- frontend/src/pages/PasswordPage.js
+++ frontend/src/pages/PasswordPage.js
@@ -1,13 +1,31 @@
 import React from 'react';
 
-import PageTitle from '../components/PageTitle';
 import PasswordReset from '../components/PasswordReset';
 
-const PasswordPage = () =>
-{
-    return(
+const PasswordPage = () => {
+    return (
         <div>
-            <PageTitle />
+            <body>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+                <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+                    <div class="container">
+                        <a class="navbar-brand">Macrotracker</a>
+
+                        <button class="navbar-toggler" type="button"
+                            data-bs-toggle="collapse" data-bs-target="#navmenu">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse" id="navmenu">
+                            <ul class="navbar-nav ms-auto">
+                                <li class="nav-item">
+                                    <a href="/" class="nav-link">Login/Register</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </body>
             <PasswordReset />
         </div>
     );
